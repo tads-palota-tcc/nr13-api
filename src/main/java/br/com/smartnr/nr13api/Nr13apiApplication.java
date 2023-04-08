637208576eb40fe1d7bb8ee254378618f74f4d8a
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-04-07 23:04:49 -0300

    commit inicial

---
+++ src/main/java/br/com/smartnr/nr13api/Nr13apiApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartnr.nr13api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nr13apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nr13apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/smartnr/nr13api/Nr13apiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartnr.nr13api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nr13apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
